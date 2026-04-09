3ea4f6cc71ee23ec1eff1285727e5ec96c04edad
khushvw <khushvant.madodiya@visionwaves.com>
2026-04-09 13:43:32 +0530

    update env var

--- service/src/main/java/com/instagram/notification_server/service/config/FirebaseConfig.java
+++ service/src/main/java/com/instagram/notification_server/service/config/FirebaseConfig.java
@@ -4,21 +4,25 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 
+import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
 import java.io.InputStream;
 
+//export FIREBASE_CONFIG="$(cat /Users/ent-00350/Documents/insta-firebase-admin.json)"
+
 @Configuration
 public class FirebaseConfig {
-
     @PostConstruct
     public void init() {
         try {
 
+            String firebaseConfig = System.getenv("FIREBASE_CONFIG");
+
             InputStream serviceAccount =
-                    new FileInputStream("/app/firebase.json");
+                    new ByteArrayInputStream(firebaseConfig.getBytes());
 
 
             FirebaseOptions options = FirebaseOptions.builder()
