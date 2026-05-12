8007a8ca2e605cc23ced9c0e9f40a3f8e92a98ef
khushvw <khushvant.madodiya@visionwaves.com>
2026-05-12 16:06:34 +0530

    update NotificationScheduler.java

--- service/src/main/java/com/instagram/notification_server/service/service/NotificationScheduler.java
+++ service/src/main/java/com/instagram/notification_server/service/service/NotificationScheduler.java
@@ -1,23 +1,29 @@
 package com.instagram.notification_server.service.service;
 
+import com.google.cloud.firestore.Firestore;
+import com.google.firebase.cloud.FirestoreClient;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Map;
+
+import static com.google.api.client.util.Data.mapOf;
 
 @Component
 public class NotificationScheduler {
     private final RestTemplate restTemplate = new RestTemplate();
 
-    @Scheduled(fixedRate = 10 * 60 * 5000)
+    @Scheduled(fixedRate = 10 * 60 * 1000)
     public void callNotificationApi() {
-
+        Firestore db = FirestoreClient.getFirestore();
+        String currentTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss"));
         try {
 
-            String response = restTemplate.getForObject(
-                    "https://insta-microservice.onrender.com/notification/get",
-                    String.class
-            );
-
+            String response = restTemplate.getForObject("https://insta-microservice.onrender.com/notification/get", String.class);
+            db.collection("Notification").document("ping").set(Map.of("log", "scheduler called at" + currentTime));
             System.out.println("Response = " + response);
 
         } catch (Exception e) {
